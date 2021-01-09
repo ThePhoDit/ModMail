d5daa63c0f1154bd919d7d3c03ffa9b3f419fec4
Mario <42234092+ThePhoDit@users.noreply.github.com>
2021-01-09 19:16:22 +0100

    Hosted logs

--- src/database/IDatabase.ts
+++ src/database/IDatabase.ts
@@ -1,14 +1,16 @@
-import { MessageLog, SnippetDB, UserDB } from '../lib/types/Database';
+import { LogDB, SnippetDB, UserDB } from '../lib/types/Database';
 
 export interface IDatabase {
 	getUser(id: string, channel: boolean): Promise<UserDB | null>;
 	addUser(id: string): Promise<void>;
 	boundChannel(userID: string, channelID: string): Promise<void>;
 	getSnippet(name: string): Promise<SnippetDB | null>;
-	closeChannel(id: string): Promise<MessageLog[]>;
+	closeChannel(id: string): void;
 	updateBlacklist(userID: string, action: 'add' | 'remove'): void;
 	createSnippet(name: string, creatorID: string, content: string): void;
 	deleteSnippet(name: string): void;
 	getSnippets(): Promise<SnippetDB[]>;
-	addMessage(userID: string, location: 'USER' | 'ADMIN' | 'OOT', content: string, channelID: string): Promise<void>;
+	addMessage(userID: string, location: 'USER' | 'ADMIN' | 'OOT', content: string, logID: string, images: string[] | undefined): Promise<void>;
+	getLogs(logID: string): Promise<LogDB | null>;
+	getUserLogs(userID: string): Promise<string[] | null>;
 }
--- src/events/channelDelete.ts
+++ src/events/channelDelete.ts
@@ -1,6 +1,7 @@
 import Caller from '../lib/structures/Caller';
 import { TextChannel } from 'eris';
 import { UserDB } from '../lib/types/Database';
+import MessageEmbed from '../lib/structures/MessageEmbed';
 export default async (caller: Caller, channel: TextChannel): Promise<unknown> => {
 	const category = caller.bot.getChannel(caller.category);
 	if (!category || category.type !== 4) return;
@@ -12,143 +13,12 @@ export default async (caller: Caller, channel: TextChannel): Promise<unknown> =>
 	if (!userDB) return;
 	if (userDB.channel === '0') return;
 
-	const messages: string[] = [];
-	const messagesArray = await caller.db.closeChannel(channel.id);
-	for (const msg of messagesArray) {
-		// Message author
-		const author = caller.bot.users.get(msg.userID) || await caller.utils.discord.fetchUser(msg.userID);
-
-		messages.push(`
-<div>
-		<h3 class="${msg.location}">${author ? `${author.username}#${author.discriminator}` : msg.userID} - ${msg.location === 'OOT' ? 'SERVER - Out Of Thread' : msg.location}</h3>
-		<p class="content-text">${msg.content}</p>
-		${msg.images ? `
-		<div class="files">
-				${msg.images.map((f, v) => `<a href="${f}">Image ${v}</a>`).join(' ')}
-		</div>` : ''}
-		<hr>
-</div>
-`);
-	}
+	caller.db.closeChannel(channel.id);
 
 	if (!caller.logsChannel) return;
-	await caller.utils.discord.createMessage(caller.logsChannel, `A thread from ${(channel as TextChannel).name} has been closed.`, false,
-		{ name: `${(channel as TextChannel).name}-${Date.now()}.html`, file: Buffer.from(`
-<html lang="en">
-<head>
-  	<title>ModMail Logs</title>
-  	<meta charset="UTF-8">
-  	<meta name="description" content="Log of a closed thread.">
-  	<link rel="icon" href=${caller.bot.user.dynamicAvatarURL()} type="image/icon type">
-  	<style>
-        body {
-            margin: 0;
-            font-family: Arial, Helvetica, sans-serif;
-            background-color: #2C2F33
-        }
-
-        .topnav {
-            overflow: hidden;
-            background-color: #23272A;
-        }
-
-        .topnav-title {
-            background-color: #99AAB5;
-            overflow: hidden;
-            width: 213px;
-            float: left;
-        }
-
-        .topnav-title p {
-            float: left;
-            font-size: 20px;
-            color: #000000;
-            padding-left: 16px;
-            position: center;
-        }
-
-        .topnav-links {
-            overflow: hidden;
-            float: left;
-        }
-
-        .topnav-links a {
-            float: left;
-            text-align: center;
-            padding: 20px 16px;
-            text-decoration: none;
-            font-size: 20px;
-            color: #7289DA;
-        }
-
-        .topnav-links a:hover {
-            color: white;
-        }
-
-        .main {
-            padding: 20px 20px;
-        }
-
-        h2 {
-            color: #99AAB5;
-        }
-
-        h3 {
-            color: #7289DA;
-        }
-
-        .files {
-            color: #fdc75b;
-        }
-
-        .files a {
-            color: white;
-            text-decoration: none;
-        }
-
-        .files a:hover {
-            text-decoration: white;
-        }
-
-        .content-text {
-            color: white;
-            padding-left: 20px;
-        }
-        
-        .ADMIN {
-        	color: forestgreen;
-        }
-        
-        .USER {
-        	color: orangered;
-        }
-    </style>
-</head>
-
-<body>
-
-		<div class="topnav">
-
-        <div class="topnav-title">
-            <img src="${caller.bot.user.dynamicAvatarURL()}" style="float: left; width: 63px; height: 63px">
-            <p>Thread Logs</p>
-        </div>
-
-        <div class="topnav-links">
-            <a href="https://mail.phodit.xyz">ModMail Project</a>
-            <a href="https://discord.gg/aUNhdFD">Support Server</a>
-        </div>
-
-    </div>
-    
-    <div class="main">
-    		<div>
-            <h2>Log from ${channel.guild.name}</h2>
-            <hr>
-        </div>
-        ${messages.join('')}
-		</div>
-</body>
-</html>
-`)});
+	const embed = new MessageEmbed()
+		.setTitle('Thread Closed')
+		.setColor('#FF0000')
+		.setDescription(`The thread \`${channel.name}\` has been closed.\n${process.env.LOGS_URL}/logs/${userDB.logs}`);
+	caller.utils.discord.createMessage(caller.logsChannel, { embed: embed.code });
 };

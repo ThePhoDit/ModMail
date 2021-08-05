7348c24f2315a017fa17df425fa82ccb06594414
Mario <mario.vitoon@gmail.com>
2021-08-05 22:55:47 +0200

    Update docs link

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,7 +4,7 @@ import MessageEmbed from '../lib/structures/MessageEmbed';
 export default new Command('help', async (caller, cmd) => {
 	const helpEmbed = new MessageEmbed()
 		.setTitle('ModMail by ThePhoDit')
-		.setDescription('If you want to check out the commands of the bot, read our docs at https://mail.phodit.xyz');
+		.setDescription('If you want to check out the commands of the bot, read our docs at https://modmail.phodit.xyz');
 	return caller.utils.discord.createMessage(cmd.channel.id, { embed: helpEmbed.code });
 },
 {

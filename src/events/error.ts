ae4c0dc897cc8f3b6e621beb1aceb2665fbb524e
Mario <42234092+ThePhoDit@users.noreply.github.com>
2022-04-17 19:50:47 +0200

    Create error.ts

---
+++ src/events/error.ts
@@ -0,0 +1,9 @@
+import Mail from '../lib/structures/Mail';
+
+export default async (caller: Mail, error: Error & { code?: number }, id: number): Promise<void> => {
+	if (!caller.bot) return;
+	if (error.code?.valueOf() === 1006) 
+    return console.log('[BOT] Connection reset by peer.')
+
+	console.error(error)
+};

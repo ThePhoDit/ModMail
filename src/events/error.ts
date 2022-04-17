1155132708e12c78b60244241db18120ed15721a
Mario <42234092+ThePhoDit@users.noreply.github.com>
2022-04-17 19:51:35 +0200

    Update tabs

--- src/events/error.ts
+++ src/events/error.ts
@@ -3,7 +3,7 @@ import Mail from '../lib/structures/Mail';
 export default async (caller: Mail, error: Error & { code?: number }, id: number): Promise<void> => {
 	if (!caller.bot) return;
 	if (error.code?.valueOf() === 1006) 
-    return console.log('[BOT] Connection reset by peer.')
+    		return console.log('[BOT] Connection reset by peer.')
 
 	console.error(error)
 };

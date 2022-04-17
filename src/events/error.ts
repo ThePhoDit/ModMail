ea7d423509dc1cf812617fed5251ab9f17dfe99d
Mario <42234092+ThePhoDit@users.noreply.github.com>
2022-04-17 19:57:10 +0200

    Remove unneeded id variable

--- src/events/error.ts
+++ src/events/error.ts
@@ -1,6 +1,6 @@
 import Mail from '../lib/structures/Mail';
 
-export default async (caller: Mail, error: Error & { code?: number }, id: number): Promise<void> => {
+export default async (caller: Mail, error: Error & { code?: number }): Promise<void> => {
 	if (!caller.bot) return;
 	if (error.code?.valueOf() === 1006) 
     		return console.log('[BOT] Connection reset by peer.')
